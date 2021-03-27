8bfcc229516b062324ce98b0a04534664608f6bc
Alex Butler <alexheretic@gmail.com>
2021-03-27 09:30:56 +0000

    Update apart-core to 0.3.10, release 0.25

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from dialog import OkDialog
 from gi.repository import GLib, Gtk, Gdk
 
 # App versions, "major.minor", major => new stuff, minor => fixes
-__version__ = '0.24'
+__version__ = '0.25'
 
 
 class LoadingBody(Gtk.Grid):
